19e9c7025868224e205a8730506419b7c3148c73
Trimple <andrevdovnar@gmail.com>
2020-05-12 20:44:05 +0300

    finished peripherial explanation

--- Software/Inc/implementation.c
+++ Software/Inc/implementation.c
@@ -4,41 +4,62 @@
 // ******** MCU connections overview ******** //
 /*
 	LEDs:
-		PB3-PB6 (digital outputs).
+		PB3-PB6 (digital outputs);
 
-	Motor Controller ( DRV8848 ):
-		PC6 - nSleep (digital output) - Logic high to enable device, logic low to enter low-power sleep mode;
-		PC7 - nFault (digital input with interrupt) - Pulled logic low with fault condition;
-		PA8-PA9 - AIN1-2 (TIM1 PWM CH1-2, alternate function 2) - motor 2 speed control inputs;
-		PA10-PA11 - BIN2-1 (TIM1 PWM CH3-4, alternate function 4) - motor 1 speed control inputs.
+	FD CAN (TCAN1042HGV) :
+		PB7 - STB (digital output) - TCAN standby pin;
+		PB8-9 - RX and TX respectively (FDCAN1, alternate function 9);
 
-	Radio module ( NRF24l01+ ):
-		PA5-PA7 - SCK, MISO, MOSI respectively (SPI1, alternate function 0);
-		PB0 - CE (digital output) - chip enable, logic high to turn on device;
-		PB1 - CSN (digital output) - SPI chip select;
-		PB2 - IRQ (interrupt input) - Pulled logic low with interrupt condition.
+	User SPI:
+		PC10-PC12 - SCK, MISO, MOSI respectively (SPI3, alternate function 0);
+		PD2 - CS (digital output) - user SPI chip select;
 
-	Motor 1 encoder:
-		PB3 - encoder input 2 (TIM2_CH2, alternate function 2),
-		PA15 - encoder input 1 (TIM2_CH1, alternate function 2).
+	USB:
+		PA11-PA12 - DM and DP respectively (alternate function ?? idk if it even an alternate function);
 
-	Motor 2 encoder:
-		PB4-5 encoder input 1-2 (TIM3 CH1-2, alternate function 1).
+	EEPROM:
+		PA8-PA9 - SDA and SCL respectively (I2C2, alternate function 4);
 
-	IMU ( ICM-20600 ):
-		PB13-PB15 (SPI2 SCK, MISO, MOSI, alternate function 0), PB10-PB11 (INT2, INT1, respectively, external interrupt), PB12 (CS, digital output).
+	Motor driver (DRV8701P):
+		PC8-PC9 - IN1 and IN2 respectively (TIM8 PWM CH3-4, alternate function 4) - driver control pins;
+		PB13 - nFault (digital input, external interrupt) - pulled logic low with fault condition;
+		PB14 - SNSOUT (digital input, external interrupt) - pulled logic low when the drive current hits the current chopping threshold;
+		PB15 - nSleep (digital output) - Pull logic low to put device into a low-power sleep mode with FETs High-Z;
+		PB2 - SO (ADC2 IN12) - Voltage on this pin is equal to the SP voltage times AV plus an offset;
 
-	Voltage control:
-		PA0 - input voltage ADC (ADC1 IN0, analog input).
+	Motor encoder:
+		PC6-7 - encoder input 1 and 2 respectively (TIM3 encoder mode Ch1-2, alternate function 2);
 
-	USART:
-		PB6-7 - TX and RX respectively (USART1, alternate function 0).
+	USB-UART (CP2102N-A01-GQFN20R):
+		PB10-11 - TX and RX respectively (UART3, alternate function 7);
+		PB12 - wakeup  (input or output ??) -  Remote USB wakeup interrupt input:
 
-	TIM14 - counts time with 0.1 ms precision for mistakes log
+	RS-485 (THDV1450):
+		PC4-5 - TX and RX respectively (UART1, alternate function 7);
+		PB0 - RW (digital output) - control recieve and transmit mode of the device. "1" - transiver, "0" - receiver;
+
+	BMS interface:
+		PA2-3 - TX and RX respectively (UART2, alternate function 7);
+		PA4 - wakeup (digital output) - ... wakes up BMS;
+		PA5 - nFault (digital input, external interrupt) -  Pulled logic low with fault condition;
+
+	User buttons:
+		PC13-PC15 - button 1 to 3 respectively (digital input);
+
+	User potentiometers:
+		PC1-3 - potentiometers 1 to 3 respectively (ADC1 IN7-9 respectively)
+
+	Current sensor (ACS711KEXLT-15AB-T):
+		PA0 - VIOut (ADC1 IN1) - Voltage proportional to thw flowing current;
+
+	Current sensor (INA240A1PW):
+		PA1 - OUT (ADC1 IN2) - Voltage proportional to the flowing current;
 
 	TIM15 - high speed counter for precise speed calculations.
 
 	TIM16 - timer for proper delay implementation
+
+	TIM17 - counts time with 0.1 ms precision for mistakes log
 
  */
 
--- Software/Inc/implementation.h
+++ Software/Inc/implementation.h
@@ -1,7 +1,110 @@
 #ifndef IMPLEMENTATION_H_
 #define IMPLEMENTATION_H_
 
+#include "implementaion_mistakes_codes.h"
 
+
+/*
+
+ */
+
+// Утсанавливает частоту процессора
+#ifndef SYSCLK_FREQUENCY
+	#define SYSCLK_FREQUENCY 				48000000 // Hz = 24 Mhz
+#endif /* SYSCLK_FREQUENCY */
+
+
+
+//****************************************//
+//****** Non user-adjustable defines ******//
+//****************************************//
+/*
+	@brief All defines that should not be changed contains here. Also all checks for #define mistakes happen here redefines happen here so they will be at top of any listing and won't distract programmers.
+ */
+
+/* Check for correct system clock define  */
+#if (SYSCLK_FREQUENCY > 64000000)
+	#error Clock speed define is higher than maximum value of 64 Mhz
+
+#elif (SYSCLK_FREQUENCY < 16000000)
+	#error Clock speed define is less then minimum value of 16 Mhz
+
+#endif
+
+
+// @brief Mistakes log entry form
+typedef struct{
+	uint32_t mistake_code;
+	uint32_t mistake_time_in_milliseconds;
+	uint32_t mistake_time_in_seconds;
+} mistake;
+
+/*** Global variables declaration ***/
+/*
+	@brief Declares global variables in all files that include this one.
+
+	 If there is VAR_DECLS define in file - declares and initialize variables (should be only one such file).
+	 If there is no VAR_DECLS define in file - declares all variables as extern and doesn't initialize them.
+ */
+#ifndef VAR_DEFS
+#define VAR_DEFS 1		// Make sure this file is included only once for each .c file
+
+#ifndef VAR_DECLS		// There should be one file with defined VAR_DECLS. So there will be only one file with variables initialization.
+# define _DECL extern	// If the .c file doesn't contains VAR_DECLS definition, all global variables will be declared with extern prefix and without initialization
+# define _INIT(x)
+#else
+# define _DECL			// If there is VAR_DECLS definition in .c file, all global variables will be declared without extern prefix
+# define _INIT(x)  = x	// and will be initialized with predefined values x
+#endif /* VAR_DECLS */
+
+/* Global variables should be declared as follows: "_DECL [standard variable declaration] _INIT(x);", where x is initialization value.
+ * If there is no need for variables initialization, declaration can look as: "_DECL [standard variable declaration];" */
+
+///*** Mistakes log variables ***//
+//	@brief This log is used to accumulate mistakes, that occurred during runtime, so it will be possible to debug system with debugger.
+_DECL mistake mistakes_log[MISTAKES_LOG_SIZE];
+_DECL uint32_t mistakes_log_pointer _INIT(0);
+_DECL uint32_t critical_mistake_detected _INIT(0);		/* will be 1 - if there were a critical mistake. System should be stopped. All performance critical objects should be returned to safe state and be deactivated */
+_DECL uint32_t time_from_log_enable_in_seconds _INIT(0);
+
+_DECL uint32_t delay_is_finished _INIT(0);
+
+#endif /* VAR_DEFS */
+/******************************************/
+
+enum question_answers
+{
+	no = 0,
+	yes = 1
+};
+
+//*** GPIO setup defines ***//
+#define GPIO_MODER_MSK			(3U) 	//0x11
+#define GPIO_ANALOG_IN 			(3U)	//0x11
+#define GPIO_ALTERNATE			(2U)	//0x10
+#define GPIO_DIGITAL_OUT		(1U)	//0x01
+#define GPIO_DIGITAL_IN			(0U)	//0x00
+
+#define GPIO_OSPEED_VERY_LOW	(0U)	//0x00
+#define GPIO_OSPEED_LOW			(1U)	//0x01
+#define GPIO_OSPEED_HIGH		(2U)	//0x10
+#define GPIO_OSPEED_VERY_HIGH	(3U)	//0x11
+
+#define ALTERNATE_FUNCTION_1	(1U)	//0x01
+#define ALTERNATE_FUNCTION_2	(2U)	//0x10
+
+//*** System clock prescalers ***//
+#define	PLLN_VALUE  			SYSCLK_FREQUENCY/2000000
+#define PLLR_VALUE				4
+#define PLL_OFFSET				1					// Offset  for PLLM and PLLR
+#define PLLM_VALUE_WITH_HSI		2
+
+//*** HSE state defines ***//
+#define HSE_IS_OK 		1
+#define HSE_IS_NOT_OK 	0
+
+/*** Completely random value to determine the waiting-state length ***/
+#define DUMMY_DELAY_VALUE 10000
 
 
 #endif /* IMPLEMENTATION_H_ */
